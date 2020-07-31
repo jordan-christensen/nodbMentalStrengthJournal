542cbd61b24f592ec54aa8b8fa9123cf14e347ee
Jordan Christensen <jchristensen@Jordans-MacBook-Pro.local>
2020-07-31 10:30:23 -0700

    Created Server

---
+++ server/controller.js
@@ -0,0 +1,33 @@
+const journal = require("./journal.json")
+let id = 6
+
+module.exports = {
+  getJournal: (req, res) => {
+    res.status(200).send(journal);
+  },
+  createEntry: (req, res) => {
+    const { date, time, objective, plan, strengths, weakness, motiv, anx, conc, conf, dm, note } = req.body;
+    if (!objective || !plan || !strengths || !weakness || !motiv || !anx || !conc || !conf || !dm ) {
+      res.status(405).send(`Missing required information`)
+    } else {
+      const newEntry = {
+        id,
+        date,
+        time,
+        objective,
+        plan,
+        strengths,
+        weakness,
+        motiv,
+        anx,
+        conc,
+        conf,
+        dm,
+        note
+      };
+      journal.push(newEntry);
+      id++;
+      res.status(200).send(journal)
+    }
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const ctrl = require("./controller");
+const app = express();
+const port = 5150;
+
+app.use(express.json());
+
+app.get("/api/journal", ctrl.getJournal)
+app.post("/api/journal", ctrl.createEntry)
+// app.delete("/api/journal/:id")
+// app.put("/api/journal/edit/:id")
+
+app.listen(port, () => console.log(`Synapses firing on port ${port}`))
